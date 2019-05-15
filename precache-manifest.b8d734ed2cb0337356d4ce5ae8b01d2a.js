8063524fdbff6cd817d93b7d089620dc28abbecb
Neli <diakonidzeneli@gmail.com>
2019-05-15 20:46:28 +0800

    Updates

---
+++ precache-manifest.b8d734ed2cb0337356d4ce5ae8b01d2a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e2786ef5c6812ccbb95",
+    "url": "/ISS-Live-Location/./static/css/main.2d8acdcf.chunk.css"
+  },
+  {
+    "revision": "6e2786ef5c6812ccbb95",
+    "url": "/ISS-Live-Location/./static/js/main.1723b190.chunk.js"
+  },
+  {
+    "revision": "f2cb24e0f7bca93de57a",
+    "url": "/ISS-Live-Location/./static/js/runtime~main.c1ba383e.js"
+  },
+  {
+    "revision": "bc4c5a892ff5af368465",
+    "url": "/ISS-Live-Location/./static/css/2.d2646a26.chunk.css"
+  },
+  {
+    "revision": "bc4c5a892ff5af368465",
+    "url": "/ISS-Live-Location/./static/js/2.db1cb78b.chunk.js"
+  },
+  {
+    "revision": "ef92b64d1d25f049ff4bdc98d171744a",
+    "url": "/ISS-Live-Location/./static/media/ISS.ef92b64d.jpg"
+  },
+  {
+    "revision": "12b8c75df959a1a741e868b2f2b2d2d3",
+    "url": "/ISS-Live-Location/./static/media/satellite.12b8c75d.svg"
+  },
+  {
+    "revision": "f2e224272b47fa294538dc14ffaf35cb",
+    "url": "/ISS-Live-Location/./static/media/map2.f2e22427.png"
+  },
+  {
+    "revision": "6738d77087fba8e2ebcaf4bc0ca1e857",
+    "url": "/ISS-Live-Location/./static/media/photo1.6738d770.jpg"
+  },
+  {
+    "revision": "bb9c0bc91309e83b906830a338c0f62c",
+    "url": "/ISS-Live-Location/./index.html"
+  }
+];
